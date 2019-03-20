4e27a598b56741879d7975e7ff81c6370f16f81f
Júlio Barreto <julio.costa@ccc.ufcg.edu.br>
2019-03-20 19:44:59 -0300

    removing comment

--- syscall.c
+++ syscall.c
@@ -149,7 +149,7 @@ syscall(void)
   num = curproc->tf->eax;
   if(num > 0 && num < NELEM(syscalls) && syscalls[num]) {
     curproc->tf->eax = syscalls[num]();
-    // cprintf("%s -> %d\n", syscalls_name[num-1], curproc->tf->eax);
+    cprintf("%s -> %d\n", syscalls_name[num-1], curproc->tf->eax);
   } else {
     cprintf("%d %s: unknown sys call %d\n",
             curproc->pid, curproc->name, num);
